4bd77d3a6004aa0db2fe5be713213a1e0fd1df5b
EzequiasCalvo <calvo.ezequias@gmail.com>
2022-10-28 16:58:56 +0200

    feat: add fade effect on letters

--- frontend/src/components/Letters.jsx
+++ frontend/src/components/Letters.jsx
@@ -1,20 +1,43 @@
-import { Letter, LetterList } from '../styles/Letters';
+import { Letter, LetterList } from '../styles/Letters'
+import { Fade } from '@mui/material'
 
-const abcList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z'];
+const abcList = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+]
 
-const lettersList = abcList.map((letter) =>
-    <Letter key={letter}>{letter}</Letter>
-  );
+const lettersList = abcList.map((letter) => (
+  <Fade key={letter} in={true} timeout={3000}>
+    <Letter>{letter}</Letter>
+  </Fade>
+))
 
 const Letters = () => {
-  
-    return (
-      <LetterList>
-        {lettersList}
-      </LetterList>
-    );
-  };
+  return <LetterList>{lettersList}</LetterList>
+}
 
-export default Letters;
+export default Letters
