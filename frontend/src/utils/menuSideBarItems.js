2f197327ed19d2581f0c228aee1ae6317c09eeee
andreaquiroga <andy.afqy@gmail.com>
2022-11-08 23:55:08 +0100

    Responsive Header

---
+++ frontend/src/utils/menuSideBarItems.js
@@ -0,0 +1,20 @@
+export const menuSideBarItems = [
+    {
+        text: 'Owlets (age 6-10)'       
+    },
+    { 
+        text: 'Owlets (age 11-14)' 
+    },
+    {
+        text: 'My account'
+    },
+    {
+        text: 'My contributions'
+    },
+    {
+        text: 'Help'
+    },
+    {
+        text: 'Change language'
+    }
+  ]
