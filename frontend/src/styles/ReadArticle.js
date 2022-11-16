e3b9b47223d1e854f45651573f22fd0760622a36
EzequiasCalvo <calvo.ezequias@gmail.com>
2022-11-16 12:23:14 +0100

    fix: height article reading level

--- frontend/src/styles/ReadArticle.js
+++ frontend/src/styles/ReadArticle.js
@@ -3,7 +3,6 @@ import { Accordion, Typography } from '@mui/material'
 import { white, lighterBlue, green, darkBlue } from '../theme/colors'
 
 export const Container = styled('div')({
-  height: '100vh',
   width: '100vw',
   display: 'flex',
   flexDirection: 'column',
@@ -122,30 +121,6 @@ export const EditImage = styled('img')((props) => ({
   cursor: 'pointer',
 }))
 
-// export const TopContainer = styled('div')({
-//   backgroundColor: '#A4E5EE',
-//   display: 'flex',
-//   justifyContent: 'space-between',
-//   paddingLeft: '10px',
-//   paddingRight: '10px',
-//   paddingTop: '15px',
-//   height: '55%',
-// })
-
-// export const MainContainer = styled('div')({
-//   backgroundColor: white,
-//   height: '70%',
-//   width: '100%',
-//   borderRadius: '50% 50% 0 0',
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center',
-//   zIndex: 1,
-//   position: 'relative',
-//   bottom: '33%',
-//   overflowWrap: 'break-word',
-// })
-
 export const BottomContainer = styled('div')({
   backgroundColor: '#0AAAAA',
   width: '100%',
@@ -154,8 +129,5 @@ export const BottomContainer = styled('div')({
   flexDirection: 'column',
   alignItems: 'center',
   marginTop: '3rem',
-  //   zIndex: 2,
-  // position: 'fixed',
-  //   top: 'calc(100% - 250px)',
   paddingBottom: '35px',
 })
