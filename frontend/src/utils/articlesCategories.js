d56189936a37e5ea8abc3f65f2e66b06d5a8be02
EzequiasCalvo <calvo.ezequias@gmail.com>
2022-11-16 19:33:44 +0100

    fix: natural science box

--- frontend/src/utils/articlesCategories.js
+++ frontend/src/utils/articlesCategories.js
@@ -1,63 +1,60 @@
 export const articlesCategoriesOwlets = [
   {
     title: 'Languages',
-    image: require('../assets/languagesOwlets.png')
+    image: require('../assets/languagesOwlets.png'),
   },
-  { title: 'Default',
-    image: require('../assets/mathOwlets.png') },
+  { title: 'Default', image: require('../assets/mathOwlets.png') },
   {
     title: 'Science',
-    image: require('../assets/socialSciencesOwlets.png')
+    image: require('../assets/socialSciencesOwlets.png'),
   },
   {
     title: 'Art',
-    image: require('../assets/artsOwlets.png')
+    image: require('../assets/artsOwlets.png'),
   },
   {
     title: 'Sports',
-    image: require('../assets/sportsOwlets.png')
+    image: require('../assets/sportsOwlets.png'),
   },
 ]
 
 export const articlesCategoriesOwls = [
   {
     title: 'Languages',
-    image: require('../assets/languagesOwls.png')
+    image: require('../assets/languagesOwls.png'),
   },
-  { title: 'Math',
-    image: require('../assets/mathOwls.png') },
+  { title: 'Math', image: require('../assets/mathOwls.png') },
   {
-    title: 'Natural Sciences',
-    image: require('../assets/naturalSciencesOwls.png')
+    title: 'Science',
+    image: require('../assets/naturalSciencesOwls.png'),
   },
   {
     title: 'Geography',
-    image: require('../assets/geographyOwls.png')
+    image: require('../assets/geographyOwls.png'),
   },
   {
     title: 'History',
-    image: require('../assets/historyOwls.png')
+    image: require('../assets/historyOwls.png'),
   },
-  { title: 'Ethics',
-    image: require('../assets/ethicsOwls.png') },
+  { title: 'Ethics', image: require('../assets/ethicsOwls.png') },
   {
     title: 'Visual Arts',
-    image: require('../assets/artsOwls.png')
+    image: require('../assets/artsOwls.png'),
   },
   {
     title: 'Crafts',
-    image: require('../assets/craftsOwls.png')
+    image: require('../assets/craftsOwls.png'),
   },
   {
     title: 'Music',
-    image: require('../assets/musicOwls.png')
+    image: require('../assets/musicOwls.png'),
   },
   {
     title: 'Sports',
-    image: require('../assets/sportsOwls.png')
+    image: require('../assets/sportsOwls.png'),
   },
   {
     title: 'Nutrition',
-    image: require('../assets/nutritionOwls.png')
+    image: require('../assets/nutritionOwls.png'),
   },
 ]
